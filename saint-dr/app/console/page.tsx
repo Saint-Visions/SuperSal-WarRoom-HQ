38ea673fe113a0387a445c94a2ad63bd40bd6b22
Saint-Visions <ryan@saintvisiongroup.com>
2025-07-27 23:32:33 -0700

    🐛 Fix console page module export

---
+++ saint-dr/app/console/page.tsx
@@ -0,0 +1,9 @@
+export default function ConsolePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Console</h1>
+      <p>This is your internal command console or dev interface.</p>
+    </div>
+  );
+}
+
